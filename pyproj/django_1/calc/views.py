94e94b94c871478444044448319b4b6fd7f36c13
Quast19 <shriganesh2019@iiitkottayam.ac.in>
2022-12-14 04:52:59 +0530

    Created django app for adding two numbers, as an exercise

---
+++ pyproj/django_1/calc/views.py
@@ -0,0 +1,13 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return render(request , 'home.html')
+
+def add(request):
+    val1 =  int(request.POST['num1'])
+    val2 =  int(request.POST['num2'])
+    res = val1 + val2
+    return render(request, 'result.html',{'result':res})
